874d75059ed506eb9c2da0dc219ec7437daa9c35
Gojde <48996561+Gojde@users.noreply.github.com>
2020-04-27 20:47:39 +0200

    build second page for mobile

---
+++ src/js/_pocetna.js
@@ -0,0 +1,8 @@
+document.getElementById('btnOnamaPocetna').onclick = () => {
+    location.href = './oNama.html';
+};
+
+document.getElementById('btnOnamaDetaljnijePocetna').onclick = () => {
+    location.href = './oNama.html';
+};
+
